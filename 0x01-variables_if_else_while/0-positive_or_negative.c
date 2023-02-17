37625a926cf5a7d907b84a82fd0200821113ab3b
Elijah-A-W <andylacron@gmail.com>
2023-02-17 12:04:49 +0300

    Return a randon num if its pos, neg or 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,19 +10,15 @@
 
 int main(void)
 {
-	int num;
+	int n;
 
 	srand(time(0));
-	num = rand() - RAND_MAX / 2;
-	if (num > 0)
-	{
-		printf("%i is positive \n", num);
-	} else if (num < 0)
-	{
-		printf("%i is negative \n", num);
-	} else
-	{
-		printf("%i is zero\n", num);
-	}
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive \n", n);
+	else if (n < 0)
+		printf("%i is negative \n", n);
+	else
+		printf("%i is zero\n", n);
 	return (0);
 }
