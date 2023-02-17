bae439a3bbbde2f77ba4a263ac4f2142fa3fe30b
Elijah-A-W <andylacron@gmail.com>
2023-02-17 13:37:40 +0300

    Printing nums of base 16 in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+*main - Entry code block
+*Description: Printing nums of base 16 in lowercase, followed by a new line.
+*Return: 0
+*/
+
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
